97e0d1deb3b1b65e6d08d07eb11d9621bd9d96e9
Gabriel <g.toro@alumnos.upm,es>
2018-05-25 20:20:11 +0200

    Yoyo

--- .idea/models/index.js
+++ .idea/models/index.js
@@ -4,11 +4,6 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 
-// To use SQLite data base:
-//    DATABASE_URL = sqlite:quiz.sqlite
-// To use  Heroku Postgres data base:
-//    DATABASE_URL = postgres://user:passwd@host:port/database
-
 const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
 
 const sequelize = new Sequelize(url);
@@ -20,6 +15,24 @@ sequelize.import(path.join(__dirname, 'quiz'));
 sequelize.import(path.join(__dirname,'session'));
 
 // Create tables
+sequelize.sync()
+    .then(()=>
+sequelize.models.quiz.count()
+)
+.then(count=>{
+    if(!count){
+    return sequelize.models.quiz.bulkCreate([
+        {question: "Capital de Italia", answer:"Roma"},
+        {question: "Capital de Francia", answer:"París"},
+        {question: "Capital de España", answer:"Madrid"},
+        {question: "Capital de Portugal", answer:"Lisboa"}
+    ])
+}
+})
+.catch(error=>{
+    console.log(error);
+});
+
 sequelize.sync()
 .then(() => console.log('Data Bases created successfully'))
 .catch(error => {
